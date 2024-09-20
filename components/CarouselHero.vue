11a68c38b8d845553410d4c659c1d1fdfc3d273a
AbdalmajeeDW <abd.almajed.alhashemi@gmail.com>
2024-09-20 16:02:39 +0300

    final project

--- components/CarouselHero.vue
+++ components/CarouselHero.vue
@@ -1,85 +1,124 @@
 <template>
-  <v-carousel
-    hide-delimiters
-    hide-delimiter-background
-    interval="3000"
-    cycle
-    style="margin-top: 90px"
-  >
-    <template v-slot:next="{ props }">
-      <div v-if="$i18n.locale == 'ar'" style="">
-        <v-btn v-bind="props" icon="mdi-arrow-left"></v-btn>
-      </div>
-      <div v-else>
-        <v-btn v-bind="props" icon="mdi-arrow-right"></v-btn>
-      </div>
-    </template>
-    <template v-slot:prev="{ props }">
-      <v-btn color="green" v-bind="props" style="">
-        <div v-if="$i18n.locale == 'ar'">
-          <v-icon>mdi-arrow-right</v-icon>
-        </div>
-        <div v-else><v-icon>mdi-arrow-left</v-icon></div>
-      </v-btn>
-    </template>
-
+  <v-carousel interval="6000" cycle hide-delimiter-background style="margin-top: 90px">
     <v-carousel-item>
       <div class="carousel-container">
-        <img src="/public/carousel/2.jpeg" alt="Image 1" class="carousel-img" />
-        <img src="/public/carousel/2_2.jpeg" alt="Image 2" class="carousel-img" />
+        <img src="/public/carousel/12.jpeg" class="img" alt="Image 1" />
+        <img
+          v-if="isWideScreen"
+          src="/public/carousel/121.jpeg"
+          class="img"
+          alt="Image 2"
+        />
       </div>
     </v-carousel-item>
-
-    <v-carousel-item>
+    <v-carousel-item v-if="isWideScreen2">
       <div class="carousel-container">
-        <img src="/public/carousel/1.jpeg" alt="Image 4" class="carousel-img" />
-        <img src="/public/carousel/1_1.jpeg" alt="Image 4" class="carousel-img" />
+        <img src="/public/carousel/121.jpeg" class="img" alt="Image 2" />
       </div>
     </v-carousel-item>
     <v-carousel-item>
       <div class="carousel-container">
-        <img src="/public/carousel/3.jpeg" alt="Image 4" class="carousel-img" />
-        <img src="/public/carousel/3_3.jpeg" alt="Image 4" class="carousel-img" />
+        <img src="/public/carousel/2.jpeg" class="img" alt="Image 1" />
+        <img
+          v-if="isWideScreen"
+          src="/public/carousel/2_2.jpeg"
+          class="img"
+          alt="Image 2"
+        />
+      </div>
+    </v-carousel-item>
+    <v-carousel-item v-if="isWideScreen2">
+      <div class="carousel-container">
+        <img src="/public/carousel/2_2.jpeg" class="img" alt="Image 2" />
+      </div>
+    </v-carousel-item>
+    <v-carousel-item>
+      <div class="carousel-container">
+        <img src="/public/carousel/1.jpeg" class="img" alt="Image 4" />
+        <img
+          v-if="isWideScreen"
+          src="/public/carousel/1_1.jpeg"
+          class="img"
+          alt="Image 4"
+        />
+      </div>
+    </v-carousel-item>
+    <v-carousel-item v-if="isWideScreen2">
+      <div class="carousel-container">
+        <img src="/public/carousel/1_1.jpeg" class="img" alt="Image 4" />
+      </div>
+    </v-carousel-item>
+    <v-carousel-item>
+      <div class="carousel-container">
+        <img src="/public/carousel/3.jpeg" class="img" alt="Image 4" />
+        <img
+          v-if="isWideScreen"
+          src="/public/carousel/3_3.jpeg"
+          class="img"
+          alt="Image 4"
+        />
+      </div>
+    </v-carousel-item>
+    <v-carousel-item v-if="isWideScreen2">
+      <div class="carousel-container">
+        <img src="/public/carousel/3_3.jpeg" class="img" alt="Image 4" />
       </div>
     </v-carousel-item>
   </v-carousel>
 </template>
+
 <script>
 export default {
   data() {
-    return {};
+    return {
+      screenWidth: null,
+    };
+  },
+  computed: {
+    isWideScreen() {
+      return this.screenWidth >= 900;
+    },
+    isWideScreen2() {
+      return this.screenWidth <= 900;
+    },
+  },
+  mounted() {
+    this.screenWidth = window.innerWidth;
+
+    window.addEventListener("resize", this.updateScreenWidth);
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.updateScreenWidth);
+  },
+  methods: {
+    updateScreenWidth() {
+      this.screenWidth = window.innerWidth;
+    },
   },
 };
 </script>
-<style scoped>
-.v-carousel {
-  height: 600px;
-  width: 100%;
-  overflow: hidden;
-}
 
-.v-carousel-item {
-  display: flex;
-  height: 100%;
-  overflow: hidden;
-}
-
+<style>
 .carousel-container {
-  display: flex;
   width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: auto auto;
   gap: 5px;
 }
-
-.carousel-img {
-  width: 50%;
+.carousel-container > .img {
+  object-fit: fill;
+  width: 100%;
   height: 100%;
-  object-fit: cover;
-  border-radius: 5px;
 }
 
+/* عند عرض أقل من 900px */
 @media (max-width: 900px) {
-  .carousel-img {
-    width: 100%;
+  .carousel-container {
+    grid-template-columns: auto;
   }
+}
+.v-carousel__controls {
+  display: none;
 }
 </style>
